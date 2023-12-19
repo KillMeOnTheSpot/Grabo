9c8ff601fca9032d136122e35f2055dcbf802d03
KillMeOnTheSpot <131246524+KillMeOnTheSpot@users.noreply.github.com>
2023-12-19 11:34:54 +0100

    init commit test

--- todo-list-musterloesung/App.vue
+++ todo-list-musterloesung/App.vue
@@ -26,33 +26,6 @@ export default {
 <style scoped>
  /* Remove styles from main.css if unhappy */
 </style>
-<script>
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
-
-export default {
-  components: { TodoInput, TodoList },
-  data() {
-    return {
-      todoList : []
-    }
-  },
-  methods: {
-    addTodoToList(newTodo){
-      this.todoList.push(newTodo);
-    }
-  }
-}
-</script>
-
-<template>
-  <h1>Meine Todo-Liste</h1>
-  <todo-input @newTodoAdded="addTodoToList"></todo-input>
-  <todo-list :data="todoList"></todo-list>
-</template>
-
-<style scoped>
- /* Remove styles from main.css if unhappy */
-</style>
-
-
+<!-- Github -->
+<!-- Github -->
+<!-- Github --><!-- Github --><!-- Github --><!-- Github --><!-- Github --><!-- Github -->
