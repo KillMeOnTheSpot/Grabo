7e0fef3543f8b0a1e50f5e3c09f50916c2c1714d
KillMeOnTheSpot <131246524+KillMeOnTheSpot@users.noreply.github.com>
2023-12-19 11:18:28 +0100

    Initial Comit Todolist
    Die Todolist Musterlösung aus Moodle

---
+++ todo-list-musterloesung/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input v-model="newTodo" />
+  <button @click="addNewTodo()">Add new Todo</button>
+</template>
+
+<script>
+export default {
+    emits : ['newTodoAdded'],
+    data(){
+        return {
+            newTodo : ''
+        }
+    },
+    methods: {
+        addNewTodo(){
+            this.$emit('newTodoAdded',this.newTodo);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    /* add your style here */
+</style>
---
+++ todo-list-musterloesung/TodoList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="element in data" :key="element">
+        {{element}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+}
+</script>
+
+<style scoped>
+    /* add your style here */
+</style>
