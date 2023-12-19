7be7dccfe277431e16869334ed7a12226a9dc909
KillMeOnTheSpot <131246524+KillMeOnTheSpot@users.noreply.github.com>
2023-12-19 11:27:12 +0100

    try Comit doubling

--- todo-list-musterloesung/App.vue
+++ todo-list-musterloesung/App.vue
@@ -26,4 +26,33 @@ export default {
 <style scoped>
  /* Remove styles from main.css if unhappy */
 </style>
-<!-- Github -->
+<script>
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+
+export default {
+  components: { TodoInput, TodoList },
+  data() {
+    return {
+      todoList : []
+    }
+  },
+  methods: {
+    addTodoToList(newTodo){
+      this.todoList.push(newTodo);
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>Meine Todo-Liste</h1>
+  <todo-input @newTodoAdded="addTodoToList"></todo-input>
+  <todo-list :data="todoList"></todo-list>
+</template>
+
+<style scoped>
+ /* Remove styles from main.css if unhappy */
+</style>
+
+
