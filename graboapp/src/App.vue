d642a4ac162e985b9687dfae7f559feeaad1cfbe
Snapdoor2 <132066784+Snapdoor2@users.noreply.github.com>
2023-12-20 13:39:15 +0100

    Job Card ausgelagert

--- graboapp/src/App.vue
+++ graboapp/src/App.vue
@@ -6,8 +6,11 @@
       <input v-model="inputValue" placeholder="Type something...">
       <button @click="fetchData">Search</button>
       <div v-if="responseData">
+        <!--die genaue Schleife ist von Chat GPT, iteriert gefetchten Daten und gibt sie mit v-for Schleife als List Items zurück-->
         <h2>Data from API:</h2>
-        <div>{{ responseData }}</div>
+        <ul>
+          <li v-for="(job, index) in responseData" :key="index">{{ job }}</li> 
+        </ul>
       </div>
       <div v-else>
         <p>Loading...</p>
@@ -16,7 +19,7 @@
   </header>
 
   <v-layout class="rounded rounded-md">
-    
+
     <v-navigation-drawer>
       <v-list>
         <v-list-item title="Navigation drawer"></v-list-item>
@@ -34,16 +37,16 @@
       Main Content
     </v-main>
     <v-container>
-    <v-chip closable> Chip </v-chip>
-  </v-container>
+      <v-chip closable> Chip </v-chip>
+    </v-container>
 
 
-  <v-container>
-    <v-card title="Card title" subtitle="Subtitle" text="..." variant="tonal"></v-card>
-  </v-container>
+    <v-container>
+      <v-card title="Card title" subtitle="Subtitle" text="..." variant="tonal"></v-card>
+    </v-container>
+    <JobInfoCard></JobInfoCard>
 
   </v-layout>
-
 </template>
 
 <!----------Script-------------->
@@ -51,58 +54,52 @@
 <script>
 //imports
 import axios from 'axios';
+import JobInfoCard from './components/jobInfoCard.vue';
 
 //variables
 const clientId = 'c003a37f-024f-462a-b36d-b001be4cd24a';
 
 export default {
-  data() {
-    return {
-      inputValue: '', //für das input feld zum suchen
-      responseData: null, // to store the response data
-    };
-  },
-  methods: {
-    fetchData() {
-      // Axios GET request, url durchsucht anhand des input feldes
-      const apiUrl = `https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/app/jobs?was=${this.inputValue}`;
-      axios
-        .get(apiUrl
-          , {
-            headers: {
-              'X-API-Key': clientId //API Key den wir der Api übergeben
-            }
-          })
-        .then((response) => {
-          // Handle successful response
-
-          //test
-          const filteredData = response.data.stellenangebote
-            //.filter(job => /* Your filter condition here */)
-            .map(job => job.titel);
-
-          // Join the array elements into a string
-          this.responseData = filteredData.join(', ');
-
-          console.log(this.responseData);
-          //test ende
-
-          // let text = "";
-
-          // for (let i = 0; i < response.data.stellenangebote.length; i++) {
-          //   text += response.data.stellenangebote[i].titel;
-          // }
-
-          // this.responseData = text;
-
-          // console.log(this.responseData);
-        })
-        .catch((error) => {
-          // Handle error
-          console.error("Error fetching data:", error);
-        });
+    data() {
+        return {
+            inputValue: '',
+            responseData: null, // to store the response data
+        };
     },
-  },
+    methods: {
+        fetchData() {
+            // Axios GET request, url durchsucht anhand des input feldes
+            const apiUrl = `https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/app/jobs?was=${this.inputValue}`;
+            axios
+                .get(apiUrl, {
+                headers: {
+                    'X-API-Key': clientId //API Key den wir der Api übergeben
+                }
+            })
+                .then((response) => {
+                // Handle successful response
+                //test
+                const filteredData = response.data.stellenangebote
+                    //.filter(job => /* Your filter condition here */)
+                    .map(job => job.titel);
+                // Join the array elements into a string
+                this.responseData = filteredData;
+                console.log(this.responseData);
+                //test ende
+                // let text = "";
+                // for (let i = 0; i < response.data.stellenangebote.length; i++) {
+                //   text += response.data.stellenangebote[i].titel;
+                // }
+                // this.responseData = text;
+                // console.log(this.responseData);
+            })
+                .catch((error) => {
+                // Handle error
+                console.error("Error fetching data:", error);
+            });
+        },
+    },
+    components: { JobInfoCard }
 };
 </script>
 
