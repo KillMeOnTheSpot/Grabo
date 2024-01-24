29cee87420a43a02bcdd88cf3a929eabbd945262
Snapdoor2 <132066784+Snapdoor2@users.noreply.github.com>
2024-01-24 18:07:12 +0100

    p tag removed

--- graboapp/src/components/StudInfoCard.vue
+++ graboapp/src/components/StudInfoCard.vue
@@ -6,7 +6,6 @@
             <p v-html="studInfo.studInhalt"></p>
         </v-card>
     </v-container>
-    <p v-text="studInfo.studInhalt"></p>
 </template>
 
  
@@ -28,5 +27,9 @@
 
 <!----------CSS---------->  
 <style scoped>
- 
+    /* .card{
+        box-shadow: 0px 2px 1px -1px, rgba(0, 0, 0, 0.2)
+        
+    } */
+    
 </style>
