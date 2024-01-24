caf2767b267a0cac9232deaad974976609784d74
Snapdoor2 <132066784+Snapdoor2@users.noreply.github.com>
2024-01-24 10:25:25 +0100

    Big changes coming soon

---
+++ graboapp/src/components/DynamicFilterCheckbox.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container fluid>
+        <!-- Displays whats selected, both can be selected at the same time: Vollzeit Teilzeit Card... -->
+        <v-checkbox v-model="isChecked" :label="checkboxLabel" @change="handleChange"></v-checkbox>
+    </v-container>
+</template>
+ 
+<!------------Script------------>
+<script>
+
+export default {
+    name: 'DynamicFilterCheckbox',
+    emits: ['checkboxChanged'],
+    props: {
+        checkboxId: Number,
+        checkboxLabel: String,
+        dataLocation: String,
+        dataValue: null,
+    },
+    data() {
+        return {
+            //selected: ['Vollzeit'],
+            isChecked: false,
+        };
+    },
+    methods: {
+        //emitted ein Event an App.vue wenn die Checkbox sich verändert
+        handleChange(){
+            this.$emit('checkboxChanged', { id: this.checkboxId, location: this.dataLocation, value: this.dataValue });
+        }
+    }
+}
+</script>
+
+<!-----------CSS----------->  
+<style scoped></style>
+
---
+++ graboapp/src/components/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="searchbars">
+        <v-text-field v-model="inputValueName" clearable label="Studiengang oder Uniname" variant="solo" class="searchfield"
+            placeholder="z.B. Medizin"></v-text-field>
+    </div>
+    <v-btn @click="searchData">Suchen</v-btn>
+</template>
+ 
+<!------------Script------------>
+<script>
+
+export default {
+    name: 'SearchBar',
+    emits: ['searchData'],
+    props: {
+    },
+    data() {
+        return {
+           inputValueName: '', 
+        };
+    },
+    methods: {
+        //emitted ein Event an App.vue wenn die Checkbox sich verändert
+        searchData() {
+            this.$emit('searchData', this.inputValueName);
+        }
+    }
+}
+</script>
+
+<!-----------CSS----------->  
+<style scoped>
+.searchbars {
+    display: flex;
+    width: 700px;
+}
+
+.searchfield {
+    margin-top: 1.5rem;
+    margin-right: 1rem;
+}
+</style>
